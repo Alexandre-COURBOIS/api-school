7f7a1844c1a1b5d917302049ff4575915ed37ed0
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2021-02-08 16:48:59 +0100

    [HOTFIX] taking off every use who are unavailable

--- src/Controller/ClasseController.php
+++ src/Controller/ClasseController.php
@@ -8,10 +8,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
 
 class ClasseController extends AbstractController
 {
