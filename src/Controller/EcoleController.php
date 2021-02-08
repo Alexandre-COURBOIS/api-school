6f22bd80ff4abb788e220c6f2a401de2accee079
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2021-02-08 10:55:29 +0100

    Create EcoleController && Fixtures

---
+++ src/Controller/EcoleController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\EcoleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EcoleController extends AbstractController
+{
+    /**
+     * @Route("/ecole", name="ecole", methods={"GET"})
+     * @param EcoleRepository $ecoleRepository
+     * @return Response
+     */
+    public function index(EcoleRepository $ecoleRepository): Response
+    {
+        $ecole = $ecoleRepository->findAll();
+
+        return new JsonResponse($ecole);
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,49 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Ecole;
+use App\Entity\Eleve;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @param ObjectManager $manager
+     * @param EntityManagerInterface $em
+     */
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        $faker = Factory::create('FR-fr');
+
+        $eleve = new Eleve();
+
+        for ($i = 0; $i <= 10; $i++) {
+
+            $eleve = new Eleve();
+
+            $eleve->setNom($faker->firstName);
+            $eleve->setPrenom($faker->lastName);
+            $eleve->setAge(rand(10,16));
+            $eleve->setCreatedAt(new \DateTime());
+
+            $manager->persist($eleve);
+
+            for ($j = 0; $j <= 10; $j++) {
+
+                $ecole = new Ecole();
+
+                $ecole->setNom($faker->company);
+                $ecole->setAdresse($faker->address);
+                $ecole->setCreatedAt(new \DateTime());
+
+            }
+
+            $manager->persist($ecole);
+        }
 
         $manager->flush();
     }
